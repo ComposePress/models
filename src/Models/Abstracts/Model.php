5a904749f340a321233613f15fb2ce218044ff52
Derrick Hammer <derrick@derrickhammer.com>
2018-03-02 11:15:49 -0500

    *Initial version

---
+++ src/Models/Abstracts/Model.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace ComposePress\Models\Abstracts;
+
+use ComposePress\Core\Abstracts\Component;
+
+/**
+ * Class Model
+ *
+ * @package ComposePress\Models\Abstracts
+ * @method int|false insert( string $table, array $data, array | string $format )
+ * @method int|false update()
+ * @method int|false replace( string $table, array $data, array | string $format )
+ * @method int|false delete( string $table, array $data, array | string $format )
+ * @method int|false insert_multisite( string $table, array $data, array | string $format )
+ * @method int|false update_multisite( string $table, array $data, array $where, array | string $format, array | string $where_format )
+ * @method int|false replace_multisite( string $table, array $data, array | string $format )
+ * @method int|false delete_multisite( string $table, array $data, array | string $format )
+ */
+abstract class Model extends Component {
+
+	/**
+	 *
+	 */
+	const NAME = '';
+
+	/**
+	 *
+	 */
+	const BUILD_MODE_SINGLESITE = 'single';
+	/**
+	 *
+	 */
+	const BUILD_MODE_MULTISITE = 'multiple';
+	/**
+	 *
+	 */
+	const BUILD_MODE_MULTISITE_GLOBAL = 'single_global';
+
+	/**
+	 *
+	 */
+	public function init() {
+		register_activation_hook( $this->plugin->plugin_file, [ $this, 'build' ] );
+		if ( is_multisite() && ! static::get_use_multisite_global_table() ) {
+			add_action( 'wpmu_new_blog', [ get_called_class(), 'build_new_blog' ] );
+			add_filter( 'wpmu_drop_tables', [ get_called_class(), 'update_mu_tables' ], 10, 2 );
+		}
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function get_use_multisite_global_table() {
+		return false;
+	}
+
+	/**
+	 * @param $network_wide
+	 */
+	public function build( $network_wide ) {
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		$single_site      = static::get_singlesite_enabled();
+		$multisite        = static::get_multisite_enabled();
+		$multisite_global = static::get_use_multisite_global_table();
+		if ( $single_site ) {
+			dbDelta( static::build_schema( self::BUILD_MODE_SINGLESITE ) );
+		}
+		if ( $network_wide && $multisite && ! $multisite_global ) {
+			$sites = get_sites( [ 'fields' => 'ids' ] );
+
+			if ( 0 < count( $sites ) ) {
+				foreach ( $sites as $site ) {
+					switch_to_blog( $site );
+					dbDelta( static::build_schema( self::BUILD_MODE_MULTISITE ) );
+					restore_current_blog();
+				}
+			}
+		}
+		if ( $network_wide && $multisite && $multisite_global ) {
+			dbDelta( static::build_schema( self::BUILD_MODE_MULTISITE_GLOBAL ) );
+		}
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function get_singlesite_enabled() {
+		return true;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function get_multisite_enabled() {
+		return false;
+	}
+
+	/**
+	 * @param $mode
+	 *
+	 * @return string
+	 */
+	protected function build_schema( $mode ) {
+		$schema_data = static::get_schema( $mode );
+		$schema      = $this->wpdb->prefix;
+		if ( self::BUILD_MODE_MULTISITE_GLOBAL === $mode ) {
+			$schema = $this->wpdb->base_prefix;
+		}
+		$schema      .= static::get_name() . ' (' . "\n";
+		$field_lines = [];
+		foreach ( $schema_data['fields'] as $field ) {
+			$field_line = "{$field['name']} {$field['type']} ";
+			if ( isset( $field['default'] ) && null !== $field['default'] ) {
+				$field_line .= "{$field['default']} ";
+			}
+			if ( empty( $field['is_null'] ) ) {
+				$field_line .= "NOT NULL ";
+			}
+			$field_line    = rtrim( $field_line );
+			$field_lines[] = $field_line;
+		}
+		$field_line      = 'PRIMARY KEY (';
+		$field_line      .= implode( ',', (array) $schema_data['primary_key'] );
+		$field_line      .= ')';
+		$field_lines []  = $field_line;
+		$charset_collate = $this->wpdb->get_charset_collate();
+
+		$schema .= implode( ",\n", $field_lines ) . "\m";
+		$schema .= ") {$charset_collate};";
+
+		return $schema;
+	}
+
+	/**
+	 * @param $mode
+	 *
+	 * @return mixed
+	 */
+	abstract protected function get_schema( $mode );
+
+	/**
+	 * @return string
+	 */
+	public function get_name() {
+		return static::NAME;
+	}
+
+	/**
+	 * @param $blog_id
+	 */
+	public function build_new_blog( $blog_id ) {
+		switch_to_blog( $blog_id );
+		dbDelta( static::build_schema( self::BUILD_MODE_MULTISITE ) );
+		restore_current_blog();
+	}
+
+	/**
+	 * @param $tables
+	 * @param $blog_id
+	 *
+	 * @return array
+	 */
+	public function update_mu_tables( $tables, $blog_id ) {
+		$tables[] = $this->wpdb->get_blog_prefix( $blog_id ) . static::get_name();
+
+		return $tables;
+	}
+
+	/**
+	 * @param $name
+	 * @param $arguments
+	 *
+	 * @return mixed
+	 */
+	public function __call( $name, $arguments ) {
+		if ( in_array( $name, [ 'insert', 'update', 'replace', 'delete' ] ) ) {
+			array_unshift( $arguments, $this->wpdb->prefix . static::get_name() );
+
+			return call_user_func_array( $this->wpdb, $arguments );
+		}
+
+		if ( in_array( $name, [ 'insert_multisite', 'update_multisite', 'replace_multisite', 'delete_multisite' ] ) ) {
+			array_unshift( $arguments, $this->wpdb->base_prefix . static::get_name() );
+
+			return call_user_func_array( $this->wpdb, $arguments );
+		}
+
+		return false;
+	}
+
+}
