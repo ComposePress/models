368503b330af2500defbc770b90ee65874eff1a9
Derrick Hammer <derrick@derrickhammer.com>
2018-03-03 21:48:52 -0500

    *Associative check ran on wrong variable

--- src/Models/Abstracts/Model.php
+++ src/Models/Abstracts/Model.php
@@ -170,7 +170,7 @@ abstract class Model extends Component {
 			$primary_used = false;
 			foreach ( $schema_data['keys'] as $key => $properties ) {
 				$field_line = '';
-				$assoc      = array_values( $field['length'] ) !== $field['length'];
+				$assoc      = array_values( $properties ) !== $properties;
 				$unique     = ! empty( $properties['unique'] );
 				$primary    = ! empty( $properties['primary'] );
 				if ( $primary && $primary_used ) {
